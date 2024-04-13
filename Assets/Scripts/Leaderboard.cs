9b155b223eca515d827263d72a1ebce3c31da125
nzKieran <syber_kierandavidson@hotmail.com>
2024-04-13 17:23:34 +1200

    working leaderboard

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -11,11 +11,17 @@ public class Leaderboard : MonoBehaviour
     [SerializeField]
     private List<TextMeshProUGUI> scores;
     public string publicKey = "2f8634f53f8d6c2374430af25637b782ba77075570c8530acd9a9d1799781689";
+
+    private void Start()
+    {
+        GetLeaderboard();
+    }
     
     public void GetLeaderboard()
     {
         LeaderboardCreator.GetLeaderboard(publicKey, ((msg) => {
-            for (int i = 0; i < names.Count; ++i) {
+            int loopLength = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLength; ++i) {
                 names[i].text = msg[i].Username;
                 scores[i].text = msg[i].Score.ToString();
             }
